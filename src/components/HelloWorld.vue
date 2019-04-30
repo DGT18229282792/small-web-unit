0613faf7ca4aef8c036fa912ce0221885ee2d13e
wowo <wowo@HFYdeMac-Pro.local>
2019-04-30 16:06:59 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="name">
+      nihai
+      <ul class="name1">
+        <li class="name3">1</li>
+        <li class="name3">2</li>
+        <li class="name3">3</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+import page from '@/components/page'
+import { Tab, TabItem, XButton, Swiper, SwiperItem} from 'vux'
+const list = () => ['精选', '美食']
+export default {
+  data () {
+    return {      
+
+    }
+  },
+  components:{
+    page,  
+    Tab,
+    TabItem,
+    XButton,
+    Swiper,
+    SwiperItem
+  }
+}
+</script>
+<style lang="scss" scoped="" type="text/css">
+
+.name{
+  color: red
+}
+</style>
+
+
+
+
---
+++ src/components/page.vue
@@ -0,0 +1,15 @@
+<!--  -->
+<template>
+  <div>我是子页面！！</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    };
+  },
+}
+
+</script>
